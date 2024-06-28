f7e23db752a78a065d4d6e6ed6469bf50a2d09ac
Bardari25 <bardaari@gmail.com>
2024-06-28 14:37:54 +0300

    G

--- Assets/Scripts/mouseMovement.cs
+++ Assets/Scripts/mouseMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class mouseMovement : MonoBehaviour
 {
-    public float mouseSensitivity = 500f;
+    public float mouseSensitivity = 400f;
 
     float xRotation = 0f;
     float yRotation = 0f;
