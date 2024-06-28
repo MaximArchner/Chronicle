bb73010978ee7b271162b4f0e7822a1b2ee77984
MaximArchner <95312458+MaximArchner@users.noreply.github.com>
2024-06-28 15:30:47 +0300

    Add files via upload

---
+++ Assets1/Scripts/mouseMovement.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouseMovement : MonoBehaviour
+{
+    public float mouseSensitivity = 100f;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        // yukarý aþaðý bakma mekaniði
+        xRotation -= mouseY;
+
+        // bakýþ açýsýnýn dönüþ sýnýrýný belirlemek için (sonsuza kadar yukarý veya aþaðý bakmamak için)
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        // saða sola bakma mekaniði
+        yRotation += mouseX;
+
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+}
+}
