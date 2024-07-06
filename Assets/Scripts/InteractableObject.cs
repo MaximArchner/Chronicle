09dd7a62367b470eaf8e351f05717db5e8164808
MaximArchner <berkeaytan@gmail.com>
2024-07-06 20:31:38 +0300

    Revert "ProximityTest Looks At Camera"
    This reverts commit 1df7bace6d423994074b819644bdbf8cb1cd6fc4.

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -19,14 +19,7 @@ public class InteractableObject : MonoBehaviour
 
     void Update()
     {
-        if (playerInRange)
-        {
-            proximityText.transform.position = transform.position + textOffset;
-            proximityText.transform.LookAt(player);
-            proximityText.transform.Rotate(0, 180, 0);
-        }
-        
-        if (Input.GetKeyDown(KeyCode.E) && playerInRange && CompareTag("Collectible")) // Objenin collider'ina dokunuyorken ve objenin tag'i Collectible ise
+        if(Input.GetKeyDown(KeyCode.E) && playerInRange && CompareTag("Collectible")) // Objenin collider'ina dokunuyorken ve objenin tag'i Collectible ise
         {
             if (!InventorySystem.Instance.CheckIfFull())
             {
