8015245aca1a12362c93395cb3756de6b1b42f62
MaximArchner <berkeaytan@gmail.com>
2024-07-06 20:57:24 +0300

    Proximity Text Fix (Again)

--- Assets/Scripts/InteractableObject.cs
+++ Assets/Scripts/InteractableObject.cs
@@ -32,6 +32,13 @@ public class InteractableObject : MonoBehaviour
                 Debug.Log("Inventory is full!");
             }
         }
+
+        if (playerInRange)
+        {
+            proximityText.transform.position = proximityText.transform.parent.position + textOffset;
+            proximityText.transform.LookAt(player.transform);
+            proximityText.transform.Rotate(0, 180, 0);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
