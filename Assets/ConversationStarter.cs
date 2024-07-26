598d92302778c404c91154d4a834f2347587cc2a
Deniz YILDIZ <100310188+denisyldz@users.noreply.github.com>
2024-07-26 15:04:56 +0300

    fix npc dialogue

--- Assets/ConversationStarter.cs
+++ Assets/ConversationStarter.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DialogueEditor;
+
 public class ConversationStarter : MonoBehaviour
 {
     [SerializeField] private NPCConversation myConversation;
+    [SerializeField] private MonoBehaviour cameraController; 
 
     private void OnTriggerStay(Collider other)
     {
@@ -12,8 +14,36 @@ public class ConversationStarter : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.F))
             {
-                ConversationManager.Instance.StartConversation(myConversation);
+                StartConversation();
             }
         }
+    }
+
+    private void StartConversation()
+    {
+        if (cameraController != null)
+        {
+            cameraController.enabled = false; 
+        }
+
+        Cursor.lockState = CursorLockMode.None; 
+        Cursor.visible = true; 
+
+        ConversationManager.Instance.StartConversation(myConversation);
+
+        ConversationManager.OnConversationEnded += EndConversation;
+    }
+
+    private void EndConversation()
+    {
+        if (cameraController != null)
+        {
+            cameraController.enabled = true; 
+        }
+
+        Cursor.lockState = CursorLockMode.Locked; 
+        Cursor.visible = false; 
+
+        ConversationManager.OnConversationEnded -= EndConversation;
     }
 }
